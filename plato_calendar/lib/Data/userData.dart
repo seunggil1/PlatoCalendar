4bcc1ca86bfe0ea39a11d4b04a2a5d9dd311bee1
seung <38664481+seunggil1@users.noreply.github.com>
2021-03-13 01:53:14 +0900

    Update userData.dart

--- plato_calendar/lib/Data/userData.dart
+++ plato_calendar/lib/Data/userData.dart
@@ -20,15 +20,16 @@ String semester = "10";
 /// 마지막 동기화 시간, Default : DateTime(1999)
 DateTime lastSyncTime;
 
+Set<CalendarData> data = {};
 /// 이전 날짜가 먼저 오는 CalendarData set
-SplayTreeSet<CalendarData> data = SplayTreeSet<CalendarData>(
-  (CalendarData a, CalendarData b) {
-    int i = a.end.compareTo(b.end);
-    if(i == 0)
-      return a.summary.compareTo(b.summary);
-    else
-      return i;
-  }); 
+// SplayTreeSet<CalendarData> data = SplayTreeSet<CalendarData>(
+//   (CalendarData a, CalendarData b) {
+//     int i = a.end.compareTo(b.end);
+//     if(i == 0)
+//       return a.summary.compareTo(b.summary);
+//     else
+//       return i;
+//   }); 
 
 /// 이번학기 수강하는 subjectCode
 Set<String> subjectCodeThisSemester;
