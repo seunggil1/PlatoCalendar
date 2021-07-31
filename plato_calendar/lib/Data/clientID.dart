92b042cfe5cebdefc3d61038e24b287fe232aa7c
seung <38664481+seunggil1@users.noreply.github.com>
2021-07-31 17:50:38 +0900

    Create clientID.dart

---
+++ plato_calendar/lib/Data/clientID.dart
@@ -0,0 +1,15 @@
+class Key{
+  //
+  // This is test key. Do not use this.
+  //
+
+  
+  /// google admob key1
+  String adUnitId1 = "ca-app-pub-3940256099942544/6300978111";
+  /// google admob key2
+  String adUnitId2 = "ca-app-pub-3940256099942544/6300978111";
+
+  /// google Cloud Platform client ID
+  String clientIDIdentifier = "sample.apps.googleusercontent.com";
+
+}
