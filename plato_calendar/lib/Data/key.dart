ac4bbe72dcc860bf13a7b194a910da24a48e0b9f
seung <38664481+seunggil1@users.noreply.github.com>
2021-07-31 17:55:25 +0900

    fileName change : clientID -> key

--- plato_calendar/lib/Data/key.dart
+++ plato_calendar/lib/Data/key.dart
@@ -3,7 +3,6 @@ class Key{
   // This is test key. Do not use this.
   //
 
-  
   /// google admob key1
   static const String adUnitId1 = "ca-app-pub-3940256099942544/6300978111";
   /// google admob key2
@@ -11,5 +10,5 @@ class Key{
 
   /// google Cloud Platform client ID
   static const String clientIDIdentifier = "sample.apps.googleusercontent.com";
-
+  
 }
