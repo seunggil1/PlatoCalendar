e5a92f055ac76dc4bdb934ae808883eef66799d4
seung <38664481+seunggil1@users.noreply.github.com>
2021-08-31 03:43:04 +0900

    v1.0.3
    - SplayTreeSet -> OrderedSet
    - Plato login request 헤더 수정
    - google calendar 연동할 때, 과목 정보 추가
    - 공작발 과목 추가
    - Accordion menu 메뉴 버그 수정
    - 오류 메세지 수정

--- plato_calendar/lib/Page/toDoList.dart
+++ plato_calendar/lib/Page/toDoList.dart
@@ -373,7 +373,7 @@ class _ToDoList extends State<ToDoList>{
                 UserData.showToDoListByIndex(index, !UserData.showToDoList[index]);
               });
             },
-            child: Icon(UserData.showToDoList[index] ? Icons.keyboard_arrow_up_sharp : Icons.keyboard_arrow_down_sharp, color: Colors.blueAccent[100]),
+            child: Icon(UserData.showToDoList[index] ? Icons.keyboard_arrow_up_sharp : Icons.keyboard_arrow_down_sharp, color: Colors.blueAccent[100], size: 27),
           )
         ],
       )
