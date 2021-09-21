9421b7214e405aa21577f43f59232df81e4caf5a
seung <38664481+seunggil1@users.noreply.github.com>
2021-09-22 03:50:56 +0900

    Update privateKey.dart

--- plato_calendar/lib/Data/privateKey.dart
+++ plato_calendar/lib/Data/privateKey.dart
@@ -1,4 +1,10 @@
 class PrivateKey{
+  // 빌드전
+  // android\app\src\main\AndroidManifest.xml에서
+  // com.google.android.gms.ads.APPLICATION_ID 값 변경
+  //
+  // ios/Runner/Info.plist에서
+  // GADApplicationIdentifier값 변경
   //
   // This is test key. Do not use this.
   //
