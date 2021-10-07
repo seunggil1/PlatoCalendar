267dfc507c37a743b341ab4ab187a1a1ed074fc9
seung <38664481+seunggil1@users.noreply.github.com>
2021-10-08 03:46:41 +0900

    getCalendar optimization, change the background sync cycle
    - 데이터 2개 먼저 받아오고 logout 요청 보내고 파싱 시작.
    - 서버에는 90분마다 요청 보냄, 앱에서 동기화 간격 3시간 넘을 경우만 진행

--- plato_calendar/lib/pnu/pnu.dart
+++ plato_calendar/lib/pnu/pnu.dart
@@ -3,15 +3,16 @@ import '../main.dart';
 import 'onestop.dart';
 import 'plato.dart';
 
-Future<bool> update({bool force = false}) async{
+Future<bool> update({bool force = false, bool background = false}) async{
   if(UserData.id != ""){
     if(DateTime.now().day != UserData.oneStopLastSyncDay)
       if(await Onestop.login() && await Onestop.getTestTimeTable() && await Onestop.logout()){}
     UserData.oneStopLastSyncDay = DateTime.now().day;
 
-    if(force ||(DateTime.now().difference(UserData.lastSyncTime).inHours > 6)){
-      if(await Plato.login() && await Plato.getCalendar() && await Plato.logout()){
-        pnuStream.sink.add(true);
+    if(force ||(DateTime.now().difference(UserData.lastSyncTime).inHours >= 6) || (background && DateTime.now().difference(UserData.lastSyncTime).inHours >= 3)){
+      if(await Plato.login() && await Plato.getCalendar()){
+        if(!background)
+          pnuStream.sink.add(true);
         return true;
       }
       return false;
