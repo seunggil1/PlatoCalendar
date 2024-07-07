5eb79ba17f1a6c2a498ddff1937573b94f53c8d5
seung <ksgg1navercom@gmail.com>
2024-07-07 20:44:44 +0900

    etc: 프로젝트 초기화

---
+++ android/app/src/main/kotlin/com/example/plato_calendar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.plato_calendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:platocalendar/main.dart';
+import 'package:plato_calendar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
