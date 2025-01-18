d449272eb9b37d9bc7582293e63a7ef571ace6f9
seung <ksgg1navercom@gmail.com>
2025-01-19 01:23:39 +0900

    etc : Add doc comments

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,3 +1,7 @@
+// Model library
+// Model library는 앱에서 사용하는 데이터 모델을 정의합니다.
+// drift에서 사용하는 ~Companion, ~Data class를 추상화 시켜 하나로 묶는 역할도 합니다.
+
 library;
 
 export 'calendar_option.dart';
--- lib/model/table/table.dart
+++ lib/model/table/table.dart
@@ -1,7 +1,11 @@
-library;
+/// Table library
+/// Table library는 drift 데이터베이스의 테이블을 정의합니다.
+/// ~Companion 클래스는 데이터를 테이블에 삽입할 때 사용합니다.
+/// ~Data 클래스는 테이블의 데이터를 읽을 때 사용합니다.
+/// class를 정의하고, 아래 명령어로 .g.dart 파일을 생성합니다.
+/// dart run build_runner build
 
-// class를 정의하고, 아래 명령어로 .g.dart 파일을 생성합니다.
-// dart run build_runner build
+library;
 
 export 'calendar_option_table.dart';
 export 'display_option/display_option.dart';
