9109140e89c039628bc4454edc161fa91a129f0b
seung <ksgg1navercom@gmail.com>
2025-02-23 22:22:37 +0900

    feat : confirmDialog 함수 추가

--- lib/widget_util/confirm_dialog.dart
+++ lib/widget_util/confirm_dialog.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
 Future<bool> confirmDialog(BuildContext context,
-    {title = 'Exit App', content = 'Are you sure you want to exit?'}) async {
+    {title = 'Exit App',
+    content = 'Are you sure you want to exit?',
+    yesLabel = 'Yes',
+    noLabel = 'No'}) async {
   return await showDialog<bool>(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: const Text('Exit App'),
-            content: const Text('Are you sure you want to exit?'),
+            title: Text(title),
+            content: Text(content),
             actions: [
               TextButton(
                 onPressed: () => Navigator.of(context).pop(false),
-                child: const Text('No'),
+                child: Text(noLabel),
               ),
               TextButton(
                 onPressed: () => Navigator.of(context).pop(true),
-                child: const Text('Yes'),
+                child: Text(yesLabel),
               ),
             ],
           );
