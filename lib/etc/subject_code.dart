ed4b744b8e9da9230088510879363f872cdda3ce
seung <ksgg1navercom@gmail.com>
2025-02-23 22:21:37 +0900

    refactor : 기본 과목코드 '전체' 추가

--- lib/etc/subject_code.dart
+++ lib/etc/subject_code.dart
@@ -4,6 +4,7 @@ import './subject_code_2024_10.dart' as subject_code_2024_10;
 import './subject_code_legacy.dart' as subject_code_legacy;
 
 const Map<String, String> subjectCode = {
+  '전체': '전체',
   ...subject_code_legacy.subjectCode,
   ...subject_code_2024_10.subjectCode,
 };
